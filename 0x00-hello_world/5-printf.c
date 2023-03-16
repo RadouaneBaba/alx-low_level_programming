36b2f6700f74102ae99f8b5e8df084993ce30b06
RadouaneBaba <radoine.baba04@gmail.com>
2023-03-16 15:57:43 -0700

    finish

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
 /**
  * main - Entry point
  * Description: 'printf task'
- * Return Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,19 +6,14 @@
  * Description: 'size of c types'
  *
  * Return: Always 0 (Success)
- */
+*/
 
 int main(void)
 {
-char c;
-int i;
-long int li;
-long long int lli;
-float f;
-printf("Size of a char: %i byte(s)", syzeof(c));
-printf("Size of an int: %i byte(s)", syzeof(i));
-printf("Size of a long int: %i byte(s)", syzeof(li));
-printf("Size of a long long int: %i byte(s)", syzeof(lli));
-printf("Size of a float: %i byte(s)", syzeof(f));
+printf("Size of a char: 1 byte(s)\n");
+printf("Size of an int: 4 byte(s)\n");
+printf("Size of a long int: 8 byte(s)\n");
+printf("Size of a long long int: 8 byte(s)\n");
+printf("Size of a float: 4 byte(s)\n");
 return (0);
 }
