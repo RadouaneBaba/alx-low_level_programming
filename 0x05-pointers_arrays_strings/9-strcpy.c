c62f6cfb342e2528d5169cc4a92b967212ecfc05
RadouaneBaba <radoine.baba04@gmail.com>
2023-03-27 15:30:22 -0700

    last

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,11 +9,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[i] <= '\0')
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
 
-	return (*dest);
+	return (dest);
 }
