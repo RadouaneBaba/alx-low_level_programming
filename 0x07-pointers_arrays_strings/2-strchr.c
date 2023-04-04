97f2d5ef6fbb5aa2ea8915c55ef67d389e7f207e
RadouaneBaba <radoine.baba04@gmail.com>
2023-04-04 14:15:29 -0700

    test 1

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,11 +9,12 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i] != '\0' && s[i] != c)
 	{
-		if (s[i] == c)
-			return (s + i);
 		i++;
 	}
-	return (NULL);
+	if (s[i] != '\0')
+		return (s + i);
+	else
+		return (NULL);
 }
