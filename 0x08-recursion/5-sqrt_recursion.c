0a63873c1449356d23a85689dcf0c7ba3e2272ea
RadouaneBaba <radoine.baba04@gmail.com>
2023-04-06 14:38:21 -0700

    test

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - square root of a num
+ * @n: num
+ * Return: result
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 1)
+		return (1);
+}
