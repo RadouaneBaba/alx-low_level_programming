d19372a84a3ef9d5d6df66c9ffca406f29c84eab
RadouaneBaba <radoine.baba04@gmail.com>
2023-04-19 18:36:46 -0700

    2 task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,18 +15,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0)
+	if (size <= 0 || cmp == NULL)
 		return (-1);
 
-	if (cmp)
+	while (i < size)
 	{
-		while (i < size)
-		{
-			if (cmp(*(array + i)) != 0)
-				return (i);
-			i++;
-		}
-		return (-1);
+		if (cmp(*(array + i)) != 0)
+			return (i);
+		i++;
 	}
 	return (-1);
 }
