06a705db6710438ff51eb75682b6bfd2106fb432
RadouaneBaba <radoine.baba04@gmail.com>
2023-04-20 19:52:58 -0700

    last

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	while (i < n)
 	{
 		printf("%i", va_arg(args, int));
-		if (separator != NULL)
+		if (separator != NULL && i != n - 1)
 			printf("%s", separator);
 		i++;
 	}
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,16 +13,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i = 0;
 	va_list args;
+	char *s;
 
 	va_start(args, n);
 
 	while (i < n)
 	{
-		if (va_arg(args, char*))
-			printf("%s", va_arg(args, char*));
+		s = va_arg(args, char *);
+		if (s)
+			printf("%s", s);
 		else
 			printf("(nil)");
-		if (separator)
+		if (separator && i != n - 1)
 			printf("%s", separator);
 		i++;
 	}
