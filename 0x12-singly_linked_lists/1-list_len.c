d64052ec8a34cbdb2c42b6a37b7911bba6270efa
RadouaneBaba <radoine.baba04@gmail.com>
2023-04-28 17:24:01 -0700

    last

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,7 +10,7 @@
 size_t list_len(const list_t *h)
 {
 	int n = 0;
-	const list_t* l = h;
+	const list_t *l = h;
 
 	if (h == NULL)
 		return (0);
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,11 +10,8 @@ void free_list(list_t *head)
 {
 	if (head == NULL)
 		return;
-	else
-	{
-		free_list(head->next);
-		free(head->str);
-		free(head);
-	}
+	free_list(head->next);
+	free(head->str);
+	free(head);
 
 }
