82d44f27190a4c2c0a1cd672d89c2f619f500b7e
RadouaneBaba <radoine.baba04@gmail.com>
2023-05-04 13:17:57 -0700

    tasks

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * _strlen - string length
+ * @s: stirng
+ * Return: str length
+ */
+
+unsigned int _strlen(char *s)
+{
+	unsigned int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+/**
+ * _pow - power of a num
+ * @n: the num
+ * @p: power
+ * Return: result
+ */
+unsigned int _pow(unsigned int n, unsigned int p)
+{
+	unsigned int i = 1;
+	if (p == 0)
+		return (1);
+	while (p--)
+		i *= n;
+	return (i);
+}
+/**
+ * binary_to_unit - convert binary to unsigned int
+ * @b: string of binary
+ * Return: decimal int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n, i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (i < _strlen(b))
+	{
+		if (b[_strlen(b) - i - 1] == '0')
+			n += 0;
+		else if (b[_strlen(b) - i - 1] == '1')
+			n += _pow(2, i);
+		else
+			return (0);
+		i++;
+	}
+	return (n);
+
+
+}
