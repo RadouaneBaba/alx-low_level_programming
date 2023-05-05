6b5e856e8693918ed731367105a6cc906adb236c
RadouaneBaba <radoine.baba04@gmail.com>
2023-05-05 08:03:43 -0700

    first task

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_binary - printf the binary representation of a number
+ * @n: the num to convert
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned int i = 1;
+
+	if (n == 0)
+		_putchar('0');
+	while (i <= n)
+		i <<= 1;;
+
+	while (i >>= 1)
+	{
+		if (i & n)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
+}
