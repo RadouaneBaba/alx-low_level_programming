00750ea7b6595ae10403ef266eb1742f897bb7be
RadouaneBaba <radoine.baba04@gmail.com>
2023-05-09 12:18:29 -0700

    more tasks

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * _strlen - string length
+ * @s: string
+ * Return: length
+ */
+
+size_t _strlen(char *s)
+{
+	size_t i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+/**
+ * create_file - create a file
+ *
+ * @filename: name of the file
+ * @text_content: content of the file
+ *
+ * Return: 1 on success -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, a;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT);
+
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+		a = write(fd, text_content, _strlen(text_content));
+
+	if (a < 0)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
