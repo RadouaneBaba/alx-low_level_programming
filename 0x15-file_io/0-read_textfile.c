e2ce7a70c9eca19622e69c1d2191a07eeb5ca8b2
RadouaneBaba <radoine.baba04@gmail.com>
2023-05-09 12:00:24 -0700

    tasks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,13 +18,26 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		free(buf);
 		return (0);
+	}
 	a = read(fd, buf, letters);
 
 	if (a == -1)
+	{
+		free(buf);
+		close(fd);
 		return (0);
+	}
 	b = write(1, buf, a);
 	if (b == -1)
+	{
+		free(buf);
+		close(fd);
 		return (0);
+	}
+	free(buf);
+	close(fd);
 	return (b);
 }
