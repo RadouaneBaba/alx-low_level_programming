6758fce827ebaa80933f50574997a297dd45ece9
RadouaneBaba <radoine.baba04@gmail.com>
2023-05-09 12:54:09 -0700

    test

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_EXCL);
+	fd = open(filename, O_CREAT | O_RDWR | O_EXCL);
 
 	if (fd == -1)
 		return (-1);
