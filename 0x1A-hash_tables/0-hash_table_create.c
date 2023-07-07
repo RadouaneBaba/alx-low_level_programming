32f83756096db863eff691955b69c49564f7e207
RadouaneBaba <radoine.baba04@gmail.com>
2023-07-07 15:42:28 -0700

    zeor

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,8 +17,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->array = malloc(sizeof(hash_node_t) * size);
 
 	if (ht->array == NULL)
+	{
 		free(ht);
 		return (NULL);
+	}
 	while (i < size)
 	{
 		ht->array[i] = NULL;
