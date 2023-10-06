bb51ef556ace9f9953dea85c3e3543e789f4119b
RadouaneBaba <radoine.baba04@gmail.com>
2023-10-06 15:16:32 -0700

    more tasks

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives the index of a key based on hash function
+ *
+ * @key: key string
+ * @size: size of the array of hashtable
+ * Return: index int
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+	return (hash_djb2(key) % size);
