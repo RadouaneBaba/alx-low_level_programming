4a8e8df505467912845b424869a60b9488fe2fdc
RadouaneBaba <radoine.baba04@gmail.com>
2023-10-06 16:47:24 -0700

    test

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_set - add element to hash table
  * @ht: the hash table
@@ -11,15 +10,29 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *elem;
+	unsigned long int index = key_index((unsigned char *) key, ht->size);
 
 	if (!key)
 		return (0);
+	elem = ht->array[index];
+	while (elem != NULL)
+	{
+		if (strcmp(elem->key, key) == 0)
+		{
+			elem->value = value;
+			return (1);
+		}
+		elem = elem->next;
+	}
+	elem = NULL;
 	elem = malloc(sizeof(hash_node_t));
+	if (elem == NULL)
+		return (0);
 	elem->key = strdup(key);
 	elem->value = strdup(value);
 	if (elem->value == NULL || elem->key == NULL)
 		return (0);
-	elem->next = ht->array[key_index((unsigned char *) key, ht->size)];
-	ht->array[key_index((unsigned char *) key, ht->size)] = elem;
+	elem->next = ht->array[index];
+	ht->array[index] = elem;
 	return (1);
 }
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieve a value associated with a key
+ * @ht: hash_table
+ * @key: key to search for
+ * Return: value or NULL
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *elem = ht->array[key_index((unsigned char *) key, ht->size)];
+
+	if (elem == NULL)
+		return (NULL);
+	if (strcmp(elem->key, key) == 0)
+		return (elem->value);
+	while (elem != NULL)
+	{
+		if (strcmp(elem->key, key) == 0)
+			return (elem->value);
+		elem = elem->next;
+	}
+	return (NULL);
+}
