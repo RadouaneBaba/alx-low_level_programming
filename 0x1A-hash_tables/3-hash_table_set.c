e86333e86556e13c3f8836b034b7c7641e811bd5
RadouaneBaba <radoine.baba04@gmail.com>
2023-10-06 15:46:31 -0700

    add elem

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - add element to hash table
+ * @ht: the hash table
+ * @key: key of the new element
+ * @value: value corresponding to key
+ * Return: 1 on success 0 on failure
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *elem;
+	unsigned long int i = 0;
+
+	if (!key)
+		return (0);
+	elem = malloc(sizeof(hash_node_t));
+	elem->key = strdup(key);
+	elem->value = strdup(value);
+	if (elem->value == NULL || elem->key == NULL)
+		return (0);
+	elem->next = ht->array[key_index(key, ht->size)];
+	ht->array[key_index(key, ht->size)] = elem;
+	return (1);
+}
