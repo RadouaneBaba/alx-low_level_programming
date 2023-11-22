3c0767d7c38706e385f4b5ac83034ae8bc8d288a
RadouaneBaba <radoine.baba04@gmail.com>
2023-11-22 13:42:03 -0800

    final

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" module of island perimeter """
+
+
+def island_perimeter(grid):
+    """ return perimter of island described by grid """
+    p = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+                if grid[i][j] == 1:
+                    p += 4
+                    if i != 0:
+                        p -= grid[i - 1][j]
+                    if i != len(grid):
+                        p -= grid[i + 1][j]
+                    if j != 0:
+                        p -= grid[i][j - 1]
+                    if j != len(grid[i]):
+                        p -= grid[i][j + 1]
+
+    return p
